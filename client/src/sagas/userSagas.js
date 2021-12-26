9ffa18b3130682142d15185ff6cb7e549298de48
Mousam Dhakal <mousamdhakal7.md@gmail.com>
2021-12-26 19:09:03 +0545

    Fix token issue

--- client/src/sagas/userSagas.js
+++ client/src/sagas/userSagas.js
@@ -1,5 +1,6 @@
 import { call, put, takeLatest } from 'redux-saga/effects'
 import { register, login } from '../services/http'
+import { setAuthorizationToken } from '../utils/authorizationHeader';
 
 function* registerUser(action) {
   try { 
@@ -16,6 +17,7 @@ function* loginUser(action) {
     const response = yield call(login, action.payload);
     if(response.data.token){
       localStorage.setItem('jwtToken', response.data.token);
+      setAuthorizationToken(response.data.token);
     }
     yield put({ type: "LOGIN_SUCCESS", payload: response.data });
     action.history.push('/');

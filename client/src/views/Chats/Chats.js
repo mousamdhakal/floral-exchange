3fe13cd5351dd7372db4e1f4cc305a93462b0c9b
Mousam Dhakal <mousamdhakal7.md@gmail.com>
2021-12-26 10:07:41 +0545

    Initialize socketio setup

--- client/src/views/Chats/Chats.js
+++ client/src/views/Chats/Chats.js
@@ -1,11 +1,16 @@
 import { useDispatch } from 'react-redux';
+import io from 'socket.io-client'
 
 import * as uiActions from '../../actions/uiActions'
+
+let socket;
 
 const Chats = () => {
   const dispatch = useDispatch();
   dispatch(uiActions.setActive('chats'));
-  
+  const ENDPOINT = 'http://localhost:5022';
+  socket = io(ENDPOINT);
+
   return (
     <div>
       <p>Chats</p>
--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
+const http = require('http')
+const { Server } = require("socket.io");
 
 const {
   bodyParserHandler,
@@ -24,6 +26,23 @@ const app = express()
 // Handle cors error
 app.use(cors())
 
+// Create http server
+const server = http.createServer(app);
+
+// Initialize socket.io
+const io = new Server(server,{
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => console.log('user disconnected'));
+});
+
 // Parse json bodies (as sent by API clients)
 app.use(express.json())
 
@@ -44,7 +63,7 @@ db.on('error', console.error.bind(console, 'connection error:'))
 
 db.once('open', function () {
   // Start listening on the port specified in the .env file
-  app.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
   })
 })

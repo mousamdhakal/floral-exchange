2a30a31fc804b8dff5faf75161c350421edbee68
Mousam Dhakal <mousamdhakal7.md@gmail.com>
2022-03-19 14:50:23 +0545

    Add logo

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,27 +1,65 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux'
 import './Header.scss'
 
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import AddRoundedIcon from '@mui/icons-material/AddRounded';
-import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
-import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
-import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
-import HomeIcon from '@mui/icons-material/Home';
-import Logout from '../Logout/Logout';
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import AddRoundedIcon from '@mui/icons-material/AddRounded'
+import ChatRoundedIcon from '@mui/icons-material/ChatRounded'
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded'
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded'
+import HomeIcon from '@mui/icons-material/Home'
+import Logout from '../Logout/Logout'
 
-import NavButton from '../NavButton/NavButton';
+import NavButton from '../NavButton/NavButton'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-  let active = useSelector((state) => state.ui.active);
+  let active = useSelector((state) => state.ui.active)
 
   return (
-    <div className='header-wrapper'>
-      <NavButton activeIcon={active} name='dashboard' Icon={HomeIcon} text={'Dashboard'} />
-      <NavButton activeIcon={active} name='nearbyItems' Icon={LocationOnIcon} text={'Nearby Items'} />
-      <NavButton activeIcon={active} name='newItem' Icon={AddRoundedIcon} text={'New Item'} />
-      <NavButton activeIcon={active} name='chats' Icon={ChatRoundedIcon} text={'Chats'} />
-      <NavButton activeIcon={active} name='activity' Icon={NotificationsRoundedIcon} text={'Activity'} />
-      <NavButton activeIcon={active} name='profile' Icon={PersonRoundedIcon} text={'Profile'} />
+    <div className="header-wrapper">
+      <Link to="/">
+        <img
+          className="brand-logo"
+          src="./assets/images/logo.png"
+          alt="brand"
+        />
+      </Link>
+      <NavButton
+        activeIcon={active}
+        name="dashboard"
+        Icon={HomeIcon}
+        text={'Dashboard'}
+      />
+      <NavButton
+        activeIcon={active}
+        name="nearbyItems"
+        Icon={LocationOnIcon}
+        text={'Nearby Items'}
+      />
+      <NavButton
+        activeIcon={active}
+        name="newItem"
+        Icon={AddRoundedIcon}
+        text={'New Item'}
+      />
+      <NavButton
+        activeIcon={active}
+        name="chats"
+        Icon={ChatRoundedIcon}
+        text={'Chats'}
+      />
+      {/* <NavButton
+        activeIcon={active}
+        name="activity"
+        Icon={NotificationsRoundedIcon}
+        text={'Activity'}
+      /> */}
+      <NavButton
+        activeIcon={active}
+        name="profile"
+        Icon={PersonRoundedIcon}
+        text={'Profile'}
+      />
       <Logout />
     </div>
   )
